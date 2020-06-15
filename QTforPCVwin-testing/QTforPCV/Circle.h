550ab285eaa0f6f24f02c802c8d98aa24f41c8e2
Krzysztof <morzansiera@gmail.com>
2020-06-15 13:07:05 +0200

    Revert "Documentation"
    This reverts commit 0420b52ba5b131aa95bb88c4e871eb9af4465340.

---
+++ QTforPCVwin-testing/QTforPCV/Circle.h
@@ -0,0 +1,22 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include <QtGui>
+#include <QWidget>
+
+class Circle : public QWidget
+{
+    Q_OBJECT
+public:
+ //  MyWidget();
+
+protected:
+    void paintEvent(QPaintEvent *event);
+
+signals:
+
+public slots:
+
+};
+
+#endif // CIRCLE_H
